dc86ba2124adf83c8c9666318090be24a5e54322
Marcelo Vieira <marcelodefy15@gmail.com>
2023-04-18 14:58:01 -0300

    Media - Inserção de arquivos de imagem. Fix - Alteração da estrutura de pastas.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/index.css';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
+import LayoutSider from './components/LayoutSider';
+import Services from './pages/Services';
+import NewServices from './pages/NewService';
+import GenericHeader from './components/GenericHeader';
+import ClientProvider from './context/ClientProvider';
+import ServiceProvider from './context/ServiceProvider';
+import NewServiceRegister from './pages/NewServiceRegister';
+import Home from './pages/Home';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <LayoutSider />
+        <GenericHeader />
+        <div className={'page'}>
+          <Switch>
+            {/* <UserProvider> */}
+            <ClientProvider>
+              <ServiceProvider>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/novoservico" component={NewServices}/>
+                <Route exact path="/novoservico/registro/:id" component={NewServiceRegister}/>
+                <Route exact path="/servicos" component={Services}/>
+              </ServiceProvider>
+
+            </ClientProvider>
+            {/* </UserProvider> */}
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
